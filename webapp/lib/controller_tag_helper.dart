dfb289581a77d64383a0928a10fc364e266297cb
Luke Church <luke@church.name>
2021-01-30 15:34:44 +0000

    Add tag helper logic

--- webapp/lib/controller_tag_helper.dart
+++ webapp/lib/controller_tag_helper.dart
@@ -4,6 +4,9 @@ part of controller;
 class TagManager {
   static final TagManager _singleton = TagManager._internal();
 
+  List<new_model.Tag> _tags;
+  List<new_model.Tag> get tags => _tags;
+
   TagManager._internal();
 
   factory TagManager() => _singleton;
@@ -11,17 +14,35 @@ class TagManager {
   void addTag(new_model.Tag tag) => addTags([tag]);
 
   void addTags(List<new_model.Tag> tags) {
-    // TODO
+    for (var tag in tags) {
+      if (_tags.any((t) => t.tagId == tag.tagId)) {
+        throw "Tag consistency error: Adding tag that already exist, ${tag.tagId}";
+      }
+      _tags.add(tag);
+    }
   }
 
   void updateTag(new_model.Tag tag) => updateTags([tag]);
   void updateTags(List<new_model.Tag> tags) {
-    // TODO
+    for (var tag in tags) {
+      if (!_tags.any((t) => t.tagId == tag.tagId)) {
+        throw "Tag consistency error: Updating tag that doesn't exist, ${tag.tagId}";
+      }
+
+      _tags.removeWhere((t) => t.tagId == tag.tagId);
+      _tags.add(tag);
+    }
+
   }
 
   void removeTag(new_model.Tag tag) => updateTags([tag]);
   void removeTags(List<new_model.Tag> tags) {
-    // TODO
-  }
+    for (var tag in tags) {
+      if (!_tags.any((t) => t.tagId == tag.tagId)) {
+        throw "Tag consistency error: Updating tag that doesn't exist, ${tag.tagId}";
+      }
 
+      _tags.removeWhere((t) => t.tagId == tag.tagId);
+    }
+  }
 }
