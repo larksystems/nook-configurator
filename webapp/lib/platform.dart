15544cac4ff1902d0ff9aa2693470c75bceafea2
EB <elayabharath@gmail.com>
2021-02-11 00:08:04 +0530

    Added auth code, all common css from katikati ui lib

--- webapp/lib/platform.dart
+++ webapp/lib/platform.dart
@@ -3,7 +3,9 @@ import 'package:firebase/firestore.dart' as firestore;
 
 import 'controller.dart' as controller;
 import 'logger.dart';
-import 'package:nook/platform_constants.dart' as platform_constants;
+import 'package:katikati_ui_lib/components/platform/platform.dart' as katikati_platform;
+export 'package:katikati_ui_lib/components/platform/platform.dart';
+import 'package:katikati_ui_lib/components/platform/platform_constants.dart' as platform_constants;
 import 'package:nook/pubsub.dart';
 import 'package:nook/model.dart';
 import 'package:nook/model_firebase.dart';
@@ -18,25 +20,8 @@ PubSubClient _pubsubInstance;
 PubSubClient _uptimePubSubInstance;
 
 init() async {
-  await platform_constants.init();
-
-  firebase.initializeApp(
-      apiKey: platform_constants.apiKey,
-      authDomain: platform_constants.authDomain,
-      databaseURL: platform_constants.databaseURL,
-      projectId: platform_constants.projectId,
-      storageBucket: platform_constants.storageBucket,
-      messagingSenderId: platform_constants.messagingSenderId);
-
-  // Firebase login
-  firebaseAuth.onAuthStateChanged.listen((firebase.User user) async {
-    if (user == null) {
-      // User signed out
-      controller.command(controller.UIAction.userSignedOut, null);
-      return;
-    }
-    // User signed in
-    String photoURL = firebaseAuth.currentUser.photoURL;
+  await katikati_platform.init("assets/firebase_constants.json", (user) {
+    String photoURL = katikati_platform.firebaseAuth.currentUser.photoURL;
     if (photoURL == null) {
       photoURL = '/assets/user_image_placeholder.png';
     }
@@ -44,25 +29,9 @@ init() async {
     _docStorage = FirebaseDocStorage(_firestoreInstance);
     _pubsubInstance = new PubSubClient(platform_constants.publishUrl, user);
     controller.command(controller.UIAction.userSignedIn, new controller.UserData(user.displayName, user.email, photoURL));
+  }, (){
+    controller.command(controller.UIAction.userSignedOut, null);
   });
-}
-
-firebase.Auth get firebaseAuth => firebase.auth();
-
-/// Signs the user in.
-signIn() {
-  var provider = new firebase.GoogleAuthProvider();
-  firebaseAuth.signInWithPopup(provider);
-}
-
-/// Signs the user out.
-signOut() {
-  firebaseAuth.signOut();
-}
-
-/// Returns true if a user is signed-in.
-bool isUserSignedIn() {
-  return firebaseAuth.currentUser != null;
 }
 
 void listenForSuggestedReplies(SuggestedReplyCollectionListener listener, [OnErrorListener onErrorListener]) =>
